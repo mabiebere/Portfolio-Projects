935ba0eb765296b977abbcb146dcf84a706c6cd4
Ebere Mabi <mabiebere@yahoo.com>
2023-06-16 00:05:45 +0200

    modified file

--- dvdrentalsqlcodes.sql
+++ dvdrentalsqlcodes.sql
@@ -333,7 +333,7 @@ select * from products;
 INSERT INTO products (product_id, name, quantity_in_stock, unit_price)
 VALUES (2, 'Pork - Bacon,back Peameal', 49, 4.65);
 
-INSERT INTO products (product_id, name, quantity_in_stock, unit_price)
+insert into products (product_id, name, quantity_in_stock, unit_price)
 VALUES (3, 'Lettuce - Romain, Heart', 38, 3.35),
 	   (4, 'Brocolinni - Gaylan, Chiness', 90, 4.53),
 	   (5, 'Sauce - Ranch Dressing', 94, 1.63),
